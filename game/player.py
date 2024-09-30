a19d6d37a75e3da7bd529f6845eb9983e16b8c34
ChrisDemSas <christopher.sastropranoto@gmail.com>
2024-09-30 23:59:30 +0800

    Added Player Class

---
+++ game/player.py
@@ -0,0 +1,13 @@
+# Implementation of the Player Class.
+
+class Player:
+    """Implementation of the Player Class.
+    
+    Attributes:
+
+    """
+
+    def __init__(self, hp: int, attack: int, magic: int) -> None:
+        """Take in a hp, attack and magic skill point and initialize the class.
+        
+        """
