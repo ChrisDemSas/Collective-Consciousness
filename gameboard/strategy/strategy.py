0bd108772959e3f44ba7f023e3c9687d75b38656
ChrisDemSas <christopher.sastropranoto@gmail.com>
2024-12-03 16:43:16 +0800

    Added Strategy class

---
+++ gameboard/strategy/strategy.py
@@ -0,0 +1,26 @@
+class Strategy:
+    """
+    Initialize the Strategy class.
+    """
+
+    def __init__(self) -> None:
+        """Initialize the Strategy Class.
+        """
+
+        self.rewards = 0
+        self.states = {0: "Defect", 1: "Cooperate"}
+        self.decision = None
+
+    def forward(self) -> str:
+        """
+        Do one forward method depending on the algorithm, and change self.decision.
+        """
+
+        raise NotImplementedError
+    
+    def return_decision(self) -> str:
+        """
+        Return self.decision.
+        """
+
+        return self.decision
